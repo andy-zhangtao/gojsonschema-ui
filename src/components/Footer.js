7d4b23a69cc9c4cd80f7e4b605736db865789828
andy <andy.zhangtao@hotmail.com>
2017-10-30 16:33:25 +0800

    增加Footer
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {Row, Col} from 'antd'
+class Footer extends Component{
+  render(){
+    return(
+      <Row>
+        <Col span={24}>
+          <div style={{padding: '20px'}}/>
+          © 2017 Json-GoStruct. All rights reserved. ztao8607@gmail.com
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export default Footer
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -3,13 +3,13 @@ import { connect } from 'dva'
 import styles from './IndexPage.css'
 import GoJsonSchema from '../components/GoJsonSchema'
 import Header from '../components/Header'
-
+import Footer from '../components/Footer'
 function IndexPage () {
   return (
     <div className={styles.normal}>
       <Header/>
       <GoJsonSchema/>
-
+      <Footer/>
     </div>
   )
 }
