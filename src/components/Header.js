1fee2482f1b4523605face434c3645062522a351
andy <andy.zhangtao@hotmail.com>
2017-10-30 17:00:37 +0800

    添加github图标
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
-import React, {Component} from 'react'
-import {Row, Col} from 'antd'
-class Header extends Component{
-  render(){
+import React, { Component } from 'react'
+import { Col, Row } from 'antd'
+
+class Header extends Component {
+  render () {
     return (
       <div>
-        <Row>
+        <Row gutter={16}>
           <Col span={24}>
             <img alt='json2gostruct' src={require('../assets/json2gostruct.svg')}/>
           </Col>
+          <Col span={1} push={10}>
+            <a href='https://github.com/andy-zhangtao/gojsonschema'>
+              <img alt='github' src='https://img.shields.io/github/forks/andy-zhangtao/gojsonschema.svg?style=social'/>
+            </a>
+          </Col>
+          <Col span={1} push={11}>
+            <a href='https://github.com/andy-zhangtao/gojsonschema'>
+              <img alt='issue' src='https://img.shields.io/github/issues/andy-zhangtao/gojsonschema.svg?style=social'/>
+            </a>
+          </Col>
+
         </Row>
-        <div style={{padding: '20px'}} />
+        <div style={{padding: '20px'}}/>
       </div>
     )
   }
